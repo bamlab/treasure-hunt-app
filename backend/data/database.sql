fd2c3c01b08785d260c833843aba678404d6d402
Varal7 <varaal@gmail.com>
2016-06-19 15:11:11 +0200

    [backend] added game/user many-to-many relationship

--- backend/data/database.sql
+++ backend/data/database.sql
@@ -18,3 +18,15 @@ CREATE TABLE IF NOT EXISTS `th_game` (
   PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS `th_played_games` (
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `user_uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `game_uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `started_at` datetime NOT NULL,
+  `finished_at` datetime DEFAULT NULL,
+  `score` SMALLINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `th_played_games` ADD FOREIGN KEY (user_uuid) REFERENCES `th_user` (`uuid`);
+ALTER TABLE `th_played_games` ADD FOREIGN KEY (game_uuid) REFERENCES `th_game` (`uuid`);
