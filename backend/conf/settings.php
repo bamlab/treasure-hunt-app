5598a0e45494fe18ddfc51685ef122965d814f68
Hugo Hamon <hugo.hamon@sensiolabs.com>
2016-06-19 15:03:23 +0200

    [backend] added database configuration.

---
+++ backend/conf/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+$conf['database'] = [
+    'driver'   => 'pdo_mysql',
+    'dbname'   => 'treasure_hunt',
+    'host'     => 'localhost',
+    'user'     => 'root',
+    'password' => null,
+    'port'     => 3306,
+    'charset'  => 'utf-8',
+];
+
+return $conf;
--- backend/web/index.php
+++ backend/web/index.php
@@ -2,12 +2,18 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+$conf = require __DIR__.'/../conf/settings.php';
+
 use TreasureHunt\TreasureHunt;
+use Silex\Provider\DoctrineServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 
 Request::enableHttpMethodParameterOverride();
 
 $app = new TreasureHunt();
+$app->register(new DoctrineServiceProvider(), [
+    'db.options' => $conf['database'],
+]);
 $app['debug'] = true;
 
 $app->post('/users', function (Request $request) use ($app) {
