3dc47ca3d0360eed2296d66561f3b18dade681f0
Marek Kalnik <marekk@bamlab.fr>
2016-06-19 16:18:08 +0200

    Detect closest beacon

--- app/src/App.js
+++ app/src/App.js
@@ -1,35 +1,91 @@
 import React, { Component } from 'react';
 import {
   DeviceEventEmitter,
+  ListView,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 import Title from './components/Title';
 
 var Beacons = require('react-native-ibeacon');
 
+var region = {
+    identifier: 'BAM',
+    uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
+};
+
+Beacons.requestWhenInUseAuthorization();
+Beacons.startRangingBeaconsInRegion(region);
+Beacons.startUpdatingLocation();
+
+class BeaconView extends Component {
+  render() {
+   return (
+     <View style={styles.row}>
+       <Text style={styles.smallText}>UUID: {this.props.uuid}</Text>
+       <Text style={styles.smallText}>Major: {this.props.major}</Text>
+       <Text style={styles.smallText}>Minor: {this.props.minor}</Text>
+       <Text>RSSI: {this.props.rssi}</Text>
+       <Text>Proximity: {this.props.proximity}</Text>
+       <Text>Distance: {this.props.accuracy.toFixed(2)}m</Text>
+     </View>
+   );
+  }
+}
+
+var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+class BeaconList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: ds.cloneWithRows([])
+    };
+  };
+
+  componentWillMount() {
+    // Listen for beacon changes
+    var subscription = DeviceEventEmitter.addListener(
+      'beaconsDidRange',
+      (data) => {
+        if (data) {
+          this.setState({
+            dataSource: ds.cloneWithRows(data.beacons)
+          });
+        }
+      }
+    );
+  };
+
+  renderRow(rowData) {
+    return <BeaconView {...rowData} style={styles.row} />
+  };
+
+ render() {
+   return (
+     <View style={styles.container}>
+       <Text style={styles.headline}>All beacons in the area</Text>
+       <ListView
+         dataSource={this.state.dataSource}
+         renderRow={this.renderRow}
+       />
+     </View>
+   );
+ };
+
+
+}
+
 class App extends Component {
   componentWillMount() {
-    var region = {
-        identifier: 'Estimotes',
-        uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
-    };
-
     var subscription = DeviceEventEmitter.addListener(
       'beaconsDidRange',
       (data) => {
         this.props.beaconData = data;
       }
     );
-
-    // Request for authorization while the app is open
-    Beacons.requestWhenInUseAuthorization();
-
-    Beacons.startMonitoringForRegion(region);
-    Beacons.startRangingBeaconsInRegion(region);
-
-    Beacons.startUpdatingLocation();
   };
 
   render() {
@@ -39,6 +95,7 @@ class App extends Component {
         <Text style={styles.welcome}>
           {this.props.beaconData}
         </Text>
+        <BeaconList/>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
