6a7777205ecb3ef1148cf40b0b829252865ce69a
Hugo Hamon <hugo.hamon@sensiolabs.com>
2016-06-19 14:38:35 +0200

    [backend] defined URIs.

--- backend/web/index.php
+++ backend/web/index.php
@@ -4,11 +4,34 @@ require __DIR__.'/../vendor/autoload.php';
 
 use TreasureHunt\TreasureHunt;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+
+Request::enableHttpMethodParameterOverride();
 
 $app = new TreasureHunt();
 $app['debug'] = true;
-$app->get('/', function (Request $request) {
-    return new Response('Welcome to Treasure Hunt app!');
+
+$app->post('/users', function (Request $request) use ($app) {
+    
 });
+
+$app->get('/users/ranking', function (Request $request) use ($app) {
+
+});
+
+$app->post('/users/{id}/games', function (Request $request) {
+
+});
+
+$app->put('/users/{user}/games/{game}', function (Request $request) {
+
+});
+
+$app->get('/games', function (Request $request) {
+
+});
+
+$app->post('/games', function (Request $request) {
+
+});
+
 $app->run(Request::createFromGlobals());
