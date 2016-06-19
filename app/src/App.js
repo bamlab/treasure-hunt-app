ba01bee1c7f7f8129ba779fb4add012e9cbf5580
Marek Kalnik <marekk@bamlab.fr>
2016-06-19 16:43:31 +0200

    Refactor beacon view as a component

--- app/src/App.js
+++ app/src/App.js
@@ -6,96 +6,16 @@ import {
   Text,
   View,
 } from 'react-native';
+
 import Title from './components/Title';
-
-var Beacons = require('react-native-ibeacon');
-
-var region = {
-    identifier: 'BAM',
-    uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
-};
-
-Beacons.requestWhenInUseAuthorization();
-Beacons.startRangingBeaconsInRegion(region);
-Beacons.startUpdatingLocation();
-
-class BeaconView extends Component {
-  render() {
-   return (
-     <View style={styles.row}>
-       <Text style={styles.smallText}>UUID: {this.props.uuid}</Text>
-       <Text style={styles.smallText}>Major: {this.props.major}</Text>
-       <Text style={styles.smallText}>Minor: {this.props.minor}</Text>
-       <Text>RSSI: {this.props.rssi}</Text>
-       <Text>Proximity: {this.props.proximity}</Text>
-       <Text>Distance: {this.props.accuracy.toFixed(2)}m</Text>
-     </View>
-   );
-  }
-}
-
-var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-
-class BeaconList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dataSource: ds.cloneWithRows([])
-    };
-  };
-
-  componentWillMount() {
-    // Listen for beacon changes
-    var subscription = DeviceEventEmitter.addListener(
-      'beaconsDidRange',
-      (data) => {
-        if (data) {
-          this.setState({
-            dataSource: ds.cloneWithRows(data.beacons)
-          });
-        }
-      }
-    );
-  };
-
-  renderRow(rowData) {
-    return <BeaconView {...rowData} style={styles.row} />
-  };
-
- render() {
-   return (
-     <View style={styles.container}>
-       <Text style={styles.headline}>All beacons in the area</Text>
-       <ListView
-         dataSource={this.state.dataSource}
-         renderRow={this.renderRow}
-       />
-     </View>
-   );
- };
-
-
-}
+import ClosestBeacon from './components/ClosestBeacon';
 
 class App extends Component {
-  componentWillMount() {
-    var subscription = DeviceEventEmitter.addListener(
-      'beaconsDidRange',
-      (data) => {
-        this.props.beaconData = data;
-      }
-    );
-  };
-
   render() {
 
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {this.props.beaconData}
-        </Text>
-        <BeaconList/>
+        <ClosestBeacon/>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
---
+++ app/src/components/ClosestBeacon.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import {
+  DeviceEventEmitter,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+var Beacons = require('react-native-ibeacon');
+
+var region = {
+    identifier: 'BAM',
+    uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
+};
+
+Beacons.requestWhenInUseAuthorization();
+Beacons.startRangingBeaconsInRegion(region);
+Beacons.startUpdatingLocation();
+
+class BeaconView extends Component {
+  render() {
+   return (
+     <View style={styles.row}>
+       <Text style={styles.smallText}>UUID: {this.props.uuid}</Text>
+       <Text style={styles.smallText}>Major: {this.props.major}</Text>
+       <Text style={styles.smallText}>Minor: {this.props.minor}</Text>
+       <Text>RSSI: {this.props.rssi}</Text>
+       <Text>Proximity: {this.props.proximity}</Text>
+       <Text>Distance: {this.props.accuracy.toFixed(2)}m</Text>
+     </View>
+   );
+  }
+}
+
+class ClosestBeacon extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      beacon: {}
+    };
+  };
+
+  componentWillMount() {
+    // Listen for beacon changes
+    var subscription = DeviceEventEmitter.addListener(
+      'beaconsDidRange',
+      (data) => {
+        var beacon;
+
+        console.log(data.beacon);
+
+        if (data && data.beacons.length) {
+          if (!this.state.beacon) {
+            // initialize to a random beacon
+            // @todo : choose the closest one
+            this.setState({
+              beacon: data.beacons[0]
+            });
+
+            return;
+          }
+
+          for (var i = 0, len = data.beacons.length; i < len; i++) {
+            beacon = data.beacons[i];
+
+            if (beacon.UUID + beacon.Major + beacon.Minor == this.state.beacon.UUID + this.state.beacon.Major + this.state.beacon.Minor) {
+              this.setState({
+                beacon: beacon
+              });
+            } else if (beacon.Distance < this.state.beacon.Distance) {
+              this.setState({
+                beacon: beacon
+              });
+            }
+          }
+        }
+      }
+    );
+  };
+
+ render() {
+   if (!this.state.beacon) {
+     <View style={styles.container}>
+       <Text>Searching...</Text>
+     </View>
+   }
+
+   return (
+     <View style={styles.container}>
+      <BeaconView {...this.state.beacon} style={styles.row} />
+     </View>
+   );
+ };
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+
+export default ClosestBeacon
